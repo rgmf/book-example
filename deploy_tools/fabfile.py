bbae6d3afe5180d965ae5dc202aadd823fe38cfb
Román <rgmf@riseup.net>
2020-01-29 13:10:53 +0100

    change python version to 3

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -4,4 +4,51 @@ from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
 
 
-REPO_URL = 'https://github.com/hjwp/book-example.git'
+REPO_URL = 'https://github.com/rgmf/book-example.git'
+
+
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
+
+
+def _get_latest_source():
+    if exists('.git'):
+        run('git fetch')
+    else:
+        run(f'git clone {REPO_URL} .')
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f'git reset --hard {current_commit}')
+
+
+def _update_virtualenv():
+    run('pipenv shell')
+
+
+def _create_or_update_dotenv():
+    append('.env', 'DJANGO_DEBUG_FALSE=y')
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        new_secret = ''.join(random.SystemRandom().choices(
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
+        ))
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+
+
+def _update_static_files():
+    run('pipenv shell')
+    run('python manage.py collectstatic --noinput')
+    run('exit')
+
+
+def _update_database():
+    run('pipenv shell')
+    run('python manage.py migrate --noinput')
+    run('exit')
